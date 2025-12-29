1711acbcfdeb83eea534f943260fe33b0025dfeb
alexwhitmore <heyimalexw@gmail.com>
2025-12-29 13:13:33 +0000

    analytics

--- docs/src/app/layout.tsx
+++ docs/src/app/layout.tsx
@@ -1,17 +1,21 @@
-import { RootProvider } from 'fumadocs-ui/provider/next';
-import './global.css';
-import { Inter } from 'next/font/google';
+import { Analytics } from '@vercel/analytics/next'
+import { RootProvider } from 'fumadocs-ui/provider/next'
+import { Inter } from 'next/font/google'
+import './global.css'
 
 const inter = Inter({
   subsets: ['latin'],
-});
+})
 
 export default function Layout({ children }: LayoutProps<'/'>) {
   return (
-    <html lang="en" className={inter.className} suppressHydrationWarning>
-      <body className="flex flex-col min-h-screen">
-        <RootProvider>{children}</RootProvider>
+    <html lang='en' className={inter.className} suppressHydrationWarning>
+      <body className='flex flex-col min-h-screen'>
+        <RootProvider>
+          {children}
+          <Analytics />
+        </RootProvider>
       </body>
     </html>
-  );
+  )
 }
